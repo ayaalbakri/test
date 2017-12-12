12301c0e9782ad81a093190791fba1778e9988e2
ayaalbakri <ayaalbakri89@gmail.com>
2017-12-12 13:36:26 +0200

    change styling and image

---
+++ server.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var staticFiles = require ('serve-static');
+var session = require('express-session')
+app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+  secret: 's3Cur3',
+  name: 'sessionId'
+}))
+
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// log all api traffic to console
+app.use('api/*',req=>{
+    console.log(req);
+    next();
+});
+
+
+
+app.post('/api/login', function (req, res) {
+
+    if(req.body && req.body.email && req.body.password){
+        if(req.body.email == '123@123.123'){
+
+            if(req.body.password == '123123') {
+                var user ={
+                    name:"Alex Jones"
+                    , email:req.body.email
+                    , password:req.body.password
+                    , profilePic:"https://www.shareicon.net/data/2016/09/01/822739_user_512x512.png"
+                };
+                res.send(200, user);
+            }
+            else
+                res.send(400,{message:'hey lady, you sent me the wrong password.'});
+
+        }else
+            res.send(400,{message:'hey man, you sent me the wrong email.'});
+
+    }
+    else
+        res.send(422,{message:'yo! you miss`n some stuff!'});
+});
+
+
+var serve = staticFiles('public/', {'index': ['index.html']});
+app.use(serve);
+
+
+app.listen(3000);
+console.log("running on http://localhost:3000");
